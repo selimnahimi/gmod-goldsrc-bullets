a1f4b684e507530dbd09e6b231ef6653be51f063
HUNcamper <selimnahimi@gmail.com>
2021-06-07 20:29:29 +0200

    dummy dummy dummy! Prediction exists. Erased a very convoluted workaround for something so simple.

--- goldsrc_bullets/lua/autorun/sh_goldsrc_bullets.lua
+++ goldsrc_bullets/lua/autorun/sh_goldsrc_bullets.lua
@@ -30,29 +30,9 @@ function GoldSrcBulletCallback(player, tr, dmginfo, toCall)
         net.Broadcast()
     end
     if CLIENT or game.SinglePlayer() then
-        -- This is a bit finnicky:
-        -- The reason as to why there's a separate clientside version of this,
-        -- is to count with the possiblity of lag. Kind of like prediction, except less complicated.
-        
-        -- However for SOME reason, "EntityFireBullets" gets called a lot of times when you have a high ping.
-        -- To avoid bullet particles and sounds being spammed, we store where each bullet landed at a very
-        -- short time window (to count with something like a shotgun: multiple bullets can land at the same time),
-        -- and if a bullet would hit the (approximately) same spot twice in this period, ignore it.
-
-        local hitString = math.Round(tr.HitPos.x) .. " ".. math.Round(tr.HitPos.y) .. " " .. math.Round(tr.HitPos.z)
-        local timePassed = CurTime() - lastParticleTime > 0.1
-
-        if (timePassed || !lastParticlePosTable[hitString]) then
+        if (IsFirstTimePredicted()) then
             GoldSrcDoImpactParticle(tr.HitPos, bodyHit, GetConVar("gsrc_bullets_mode"):GetString())
             GoldSrcPlayRicochet(tr.HitPos)
-            
-            lastParticlePosTable[hitString] = true
-
-            if (timePassed) then
-                -- lets empty the bullet table, because time has already passed
-                lastParticleTime = CurTime()
-                table.Empty(lastParticlePosTable)
-            end
         end
     end
 
