a7d4d4315603d8a099f268546c2a6b389a95905c
HUNcamper <selimnahimi@gmail.com>
2021-06-08 20:54:37 +0200

    Default is now cs 1.6 effects

--- goldsrc_bullets/lua/autorun/client/cl_goldsrc_bullets.lua
+++ goldsrc_bullets/lua/autorun/client/cl_goldsrc_bullets.lua
@@ -3,7 +3,7 @@ PrecacheParticleSystem( "goldsrc_impact")
 PrecacheParticleSystem( "goldsrc_cs16_impact")
 PrecacheParticleSystem( "goldsrc_blood_impact")
 
-local cvarMode = CreateClientConVar( "gsrc_bullets_mode", "hl1", true, false)
+local cvarMode = CreateClientConVar( "gsrc_bullets_mode", "cs16", true, false)
 local cvarParticlesImpact = CreateClientConVar("gsrc_bullets_particles_impact", "1", true, false)
 local cvarParticlesBlood  = CreateClientConVar("gsrc_bullets_particles_blood", "1", true, false)
 
