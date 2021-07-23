c77a16ce39ecd4e40b2d427cccdc366885e97bab
HUNcamper <selimnahimi@gmail.com>
2021-07-23 17:53:00 +0200

    Fixed breaking VJ Base, added cvar for entire addon toggle

--- goldsrc_bullets/lua/autorun/server/sv_goldsrc_bullets.lua
+++ goldsrc_bullets/lua/autorun/server/sv_goldsrc_bullets.lua
@@ -1,20 +1,22 @@
 util.AddNetworkString("GoldSrcBulletImpact")
 
 function HeadshotHook(ent, hitgroup, dmginfo)
-    if hitgroup == HITGROUP_HEAD and GetConVar("gsrc_bullets_headshot"):GetBool() then
-        local neededArmor = GetConVar("gsrc_bullets_headshot_helmet"):GetInt()
-        local onlyPlayers = GetConVar("gsrc_bullets_headshot_players"):GetBool()
-        local choice
-        
-        if !ent:IsPlayer() and onlyPlayers then return end
+    if GetConVar("gsrc_bullets_enabled"):GetBool() then
+        if hitgroup == HITGROUP_HEAD and GetConVar("gsrc_bullets_headshot"):GetBool() then
+            local neededArmor = GetConVar("gsrc_bullets_headshot_helmet"):GetInt()
+            local onlyPlayers = GetConVar("gsrc_bullets_headshot_players"):GetBool()
+            local choice
+            
+            if !ent:IsPlayer() and onlyPlayers then return end
 
-        if ent:IsPlayer() and ent:Armor() >= neededArmor then
-            choice = "GoldSrc.Impact.Helmet"
-        else
-            choice = "GoldSrc.Impact.Headshot"
+            if ent:IsPlayer() and ent:Armor() >= neededArmor then
+                choice = "GoldSrc.Impact.Helmet"
+            else
+                choice = "GoldSrc.Impact.Headshot"
+            end
+
+            ent:EmitSound(choice)
         end
-
-        ent:EmitSound(choice)
     end
 end
 
--- goldsrc_bullets/lua/autorun/sh_goldsrc_bullets.lua
+++ goldsrc_bullets/lua/autorun/sh_goldsrc_bullets.lua
@@ -23,45 +23,63 @@ CreateConVar("gsrc_bullets_ricochet", "1", FCVAR_REPLICATE, "Enable/Disable the 
 CreateConVar("gsrc_bullets_headshot", "1", FCVAR_REPLICATE, "Enable/Disable the GoldSrc headshot sounds")
 CreateConVar("gsrc_bullets_headshot_players", "0", FCVAR_REPLICATE, "Only play headshot sounds for players")
 CreateConVar("gsrc_bullets_headshot_helmet", "50", FCVAR_REPLICATE, "Minimum armor needed to play a helmet headshot sound")
+CreateConVar("gsrc_bullets_enabled", "1", FCVAR_REPLICATE, "Enable/Disable the addon (doesn't disable impact materials!)")
 
 
 -- Callback for the fired bullet (GM:EntityFireBullets)
 function GoldSrcBulletCallback(player, tr, dmginfo, toCall)
-    local hitEnt = tr.Entity
+    if GetConVar("gsrc_bullets_enabled"):GetBool() then
+        if (toCall != nil) then toCall(player, tr, dmginfo) end
 
-    local bodyHit = false
+        local bodyHit = false
 
-    if SERVER then
-        -- Dispatch a bullet hit effect for every client
-        net.Start("GoldSrcBulletImpact")
-        net.WriteEntity(player)
-        net.WriteVector(tr.HitPos)
-        net.WriteInt(tr.MatType, 8)
-        net.WriteEntity(tr.Entity)
-        net.Broadcast()
-    end
-    if CLIENT or game.SinglePlayer() then
-        if (IsFirstTimePredicted()) then
-            GoldSrcDoImpactParticle(tr.HitPos, tr.MatType, tr.Entity, GetConVar("gsrc_bullets_mode"):GetString())
+        if SERVER then
+            -- Dispatch a bullet hit effect for every client
+            net.Start("GoldSrcBulletImpact")
+            net.WriteEntity(player)
+            net.WriteVector(tr.HitPos)
+            net.WriteInt(tr.MatType, 8)
+            net.WriteEntity(tr.Entity)
+            net.Broadcast()
+        end
+        if CLIENT or game.SinglePlayer() then
+            if (IsFirstTimePredicted()) then
+                GoldSrcDoImpactParticle(tr.HitPos, tr.MatType, tr.Entity, GetConVar("gsrc_bullets_mode"):GetString())
+            end
         end
     end
-
-    if (toCall != nil) then return toCall(player, tr, dmginfo) end
 end
 
 -- Hook for fired bullets (GM:EntityFireBullets)
-function GoldSrcBulletHook(shooter, data)
+function GoldSrcBulletHook(shooter, bullet)
+    if GetConVar("gsrc_bullets_enabled"):GetBool() then
+        local toCall = bullet.Callback
+        
+        bullet.Callback = function(player, tr, dmginfo)
+            -- This convoluted callback is needed, because some SWEPs have their own
+            -- bullet callbacks, and overwriting them isn't a good idea. Instead,
+            -- let's call our own, and once done, call the SWEP's own bullet callback
+            GoldSrcBulletCallback(player, tr, dmginfo, toCall)
+        end
 
-    local toCall = data.Callback
+        if IsValid(shooter) && shooter:IsNPC() && shooter.IsVJBaseSNPC == true then
+            local ene = shooter:GetEnemy()
+            local wep = shooter:GetActiveWeapon()
 
-    data.Callback = function(player, tr, dmginfo)
-        -- This convoluted callback is needed, because some SWEPs have their own
-        -- bullet callbacks, and overwriting them isn't a good idea. Instead,
-        -- let's call our own, and once done, call the SWEP's own bullet callback
-        return GoldSrcBulletCallback(player, tr, dmginfo, toCall)
+            local fSpread = (shooter:GetPos():Distance(ene:GetPos()) / 28) * shooter.WeaponSpread * (wep.NPC_CustomSpread or 1)
+            bullet.Spread = Vector(fSpread, fSpread, 0)
+
+            if shooter.WeaponUseEnemyEyePos == true then
+                bullet.Dir = (ene:EyePos() + ene:GetUp()*-5) - bullet.Src
+            else
+                bullet.Dir = (ene:GetPos() + ene:OBBCenter()) -  bullet.Src
+            end
+        end
+
+        --bullet.Callback = toCall
+
+        return true
     end
-
-    return true
 end
 
 -- Hooks
